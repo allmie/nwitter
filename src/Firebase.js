6b6e290a35cc19d6aeec32f6c281adad7ccb16e2
jsw <allmie@naver.com>
2021-09-14 11:11:55 +0900

    Environment varialbe

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDP5iTHC7kTqK6o1cqEm3KUZLYOwuOGNG8',
-  authDomain: 'nwitter-f1b5a.firebaseapp.com',
-  projectId: 'nwitter-f1b5a',
-  storageBucket: 'nwitter-f1b5a.appspot.com',
-  messagingSenderId: '943482850396',
-  appId: '1:943482850396:web:988975e851431ed46fea99',
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINSENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
 
 export default initializeApp(firebaseConfig);
