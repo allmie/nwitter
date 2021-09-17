54cb351d34699c30abd007eff95312cf6fdb36b5
jsw <allmie@naver.com>
2021-09-17 14:04:00 +0900

    Logout

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,21 @@
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from 'routes/Home';
 import Auth from 'routes/Auth';
+import Navigation from './Navigation';
+import Profile from 'routes/Profile';
 
 const MainRouter = ({ isLogin }) => {
   return (
     <Router>
+      {isLogin && <Navigation />}
       <Switch>
         {isLogin ? (
           <>
             <Route exact path='/'>
               <Home />
+            </Route>
+            <Route exact path='/profile'>
+              <Profile />
             </Route>
           </>
         ) : (
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,20 @@
+import { signOut } from '@firebase/auth';
+import { authService } from 'fbase';
 import React from 'react';
+import { useHistory } from 'react-router';
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+  const history = useHistory();
+  const onClickLogout = () => {
+    signOut(authService);
+    history.push('/');
+  };
+  return (
+    <>
+      <span>Profile</span>
+      <button onClick={onClickLogout}>Sign out</button>
+    </>
+  );
+};
 
 export default Profile;
